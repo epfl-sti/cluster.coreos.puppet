b89980f8f612115d1daa0615349fc518e1478268
Dominique Quatravaux <dominique@quatravaux.org>
2015-07-27 11:43:37 +0200

    Add external address and masquerading support

--- manifests/network.pp
+++ manifests/network.pp
@@ -19,32 +19,68 @@
 # [*external_netmask*]
 #   The netmask for the external network.
 #
+# [*gateway_vip*]
+#   To be set only on the router; enable that virtual IP (VIP).
+#   TODO: this should be moved to a heartbeat rig. This not made persistent
+#   for that reason.
+#
 # === Actions:
 #
 # This module sets the default route and activates IPv4 forwarding on gateway
 # nodes (those that have $external_address set).
 class epflsti_coreos::network(
+  $external_interface = "enp1s0f1",
   $external_address = undef,
   $external_gateway = undef,
   $external_netmask = undef,
+  $gateway_vip = undef
 ) {
   if ($external_address) {
     validate_string($external_gateway, $external_netmask)
   }
 
-  exec { "/usr/bin/systemctl restart systemd-networkd.service":
-    alias => "restart networkd in host",
+  exec { "restart networkd in host":
+    command => "/usr/bin/systemctl restart systemd-networkd.service",
     refreshonly => true
   }
 
   if ($external_address) {
+    file { "/etc/systemd/50-${external_interface}-epflnet.network":
+      ensure => "present",
+      contents => template("50-epflnet.network.erb")
+    } ~> Exec["restart networkd in host"]
     file { "/etc/systemd/40-ethbr4-nogateway.network":
       ensure => "link",
       target => "40-ethbr4-nogateway.opt-network"
     } ~> Exec["restart networkd in host"]
+
   } else {
     file { "/etc/systemd/40-ethbr4-nogateway.network":
       ensure => "absent"
     } ~> Exec["restart networkd in host"]
+  }
+
+  if ($gateway_vip) {
+    exec { "Enable gateway VIP":
+      path => $path,
+      command => "/sbin/ip addr add ${gateway_vip}/24 dev ethbr4",
+      unless => "/sbin/ip addr show |grep -qw ${gateway_vip}"
+    } 
+    exec { "Enable masquerading":
+      path => $path,
+      command => "/sbin/iptables -t nat -A POSTROUTING -o ${external_interface} -j MASQUERADE",
+      unless => "/sbin/iptables -t nat -L -v| grep 'MASQUERADE.*${external_interface}'"
+    } 
+  } else {
+    exec { "Disable gateway VIP":
+      path => $path,
+      command => "/sbin/ip addr del ${gateway_vip}/24",
+      unless => "! /sbin/ip addr show | grep -qw ${gateway_vip}"
+    } 
+    exec { "Disable masquerading":
+      path => $path,
+      command => "/sbin/iptables -t nat -D POSTROUTING -o ${external_interface} -j MASQUERADE",
+      unless => "! /sbin/iptables -t nat -L -v| grep 'MASQUERADE.*${external_interface}'"
+    } 
   }
 }
