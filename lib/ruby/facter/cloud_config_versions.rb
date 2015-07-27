785b4a2bdffc1a634890c74f6a21681403b25be8
domq <dominique@quatravaux.org>
2015-07-27 17:40:31 +0200

    Missing full path to cloud-config.yml

--- lib/ruby/facter/cloud_config_versions.rb
+++ lib/ruby/facter/cloud_config_versions.rb
@@ -1,12 +1,12 @@
 Facter.add('coreos_cloudconfig_provision_version') do
   setcode do
-    Facter::Core::Execution.exec("sed -ne '/provision.erb \$Id:/p' < cloud-config.yml | awk '{print $4}'")
+    Facter::Core::Execution.exec("sed -ne '/provision.erb \$Id:/p' < /etc/coreos/cloud-config.yml | awk '{print $4}'")
   end
 end
 
 Facter.add('coreos_cloudconfig_snippet_version') do
   setcode do
-    Facter::Core::Execution.exec("sed -ne '/coreos_cloudconfig.erb \$Id:/p' < cloud-config.yml | awk '{print $4}'")
+    Facter::Core::Execution.exec("sed -ne '/coreos_cloudconfig.erb \$Id:/p' < /etc/coreos/cloud-config.yml | awk '{print $4}'")
   end
 end
 
