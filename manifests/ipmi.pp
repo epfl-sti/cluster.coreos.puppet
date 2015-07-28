5b2b28598c0c06ca0eaa656c0600be5ff0755387
Dominique Quatravaux <dom@tsf-428-wpa-4-181.epfl.ch>
2015-07-28 19:33:26 +0200

    Better unless clause
    Turns out, there is actually a root password by default.

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -5,6 +5,6 @@ class epflsti_coreos::ipmi() {
   exec { "Set IPMI root password":
     command => "ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmi channel setaccess 1 2 link=on ipmi=on callin=on privilege=4 && ipmitool user enable 2",
     path => $path,
-    unless => "ipmitool user list|grep -q root"
+    unless => "ipmitool user list|grep -q 'root.*true.*true.*true'"
   }
 }
