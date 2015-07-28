36b2b0096dfaf4d2da32c2d3cc58f935436919cd
Dominique Quatravaux <dom@tsf-428-wpa-4-181.epfl.ch>
2015-07-28 21:38:55 +0200

    Bring back the zoinx

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -6,7 +6,7 @@ class epflsti_coreos::ipmi() {
     "X7DBT": {
       # Believe it or not, "ipmi=on" actually turns it off! "ipmi=zoinx" works.
       exec { "Set up IPMI remote access":
-        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=$activate ipmi=$activate callin=on privilege=4 && ipmitool user enable 2",
+        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2",
         path => $path,
         unless => "ipmitool channel getaccess 1 2|grep -q 'IPMI Messaging.*enabled'"
       }
