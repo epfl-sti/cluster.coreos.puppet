b8ef5842170a488cc4a2fe194ea0f3c06a042a2a
Dominique Quatravaux <dom@tsf-428-wpa-4-181.epfl.ch>
2015-07-28 20:29:53 +0200

    Shotgun debugging of IPMI settings
    Yes, it does look like link=zoinx ipmi=zoinx is necessary - "ipmi=on"
    turns it off :-P

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -2,9 +2,9 @@
 #
 # Configure IPMI with a root password and so on.
 class epflsti_coreos::ipmi() {
-  exec { "Set IPMI root password":
-    command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=on ipmi=on callin=on privilege=4 && ipmitool user enable 2",
+  exec { "Set up IPMI remote access":
+    command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2",
     path => $path,
-    unless => "ipmitool user list|grep -q 'root.*true.*true.*true'"
+    unless => "ipmitool channel getaccess 1 2|grep -q 'IPMI Messaging.*enabled'"
   }
 }
