1c2b3d7701a27e77dbb1b6fba9dddd69baf59434
Dominique Quatravaux <dom@tsf-428-wpa-4-181.epfl.ch>
2015-07-28 19:45:00 +0200

    Missing two ipmitool commands
    Assuming these are the reason why "link=on" has no effect.

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -3,7 +3,7 @@
 # Configure IPMI with a root password and so on.
 class epflsti_coreos::ipmi() {
   exec { "Set IPMI root password":
-    command => "ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=on ipmi=on callin=on privilege=4 && ipmitool user enable 2",
+    command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=on ipmi=on callin=on privilege=4 && ipmitool user enable 2",
     path => $path,
     unless => "ipmitool user list|grep -q 'root.*true.*true.*true'"
   }
