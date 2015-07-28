4b732b14e1f0f69a6890b2eee29b46cc134de5a5
Dominique Quatravaux <dom@tsf-428-wpa-4-181.epfl.ch>
2015-07-28 22:18:48 +0200

    Last ditch effort - Set password last
    I noticed that when doing things manually, running the ipmitool user
    set password manually once more gets things going, I think

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -6,7 +6,7 @@ class epflsti_coreos::ipmi() {
     "X7DBT": {
       # Believe it or not, "ipmi=on" actually turns it off! "ipmi=zoinx" works.
       exec { "Set up IPMI remote access":
-        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2",
+        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root&& ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2  && ipmitool user set password 2 ${::ipmi_root_password}",
         path => $path,
         unless => "ipmitool channel getaccess 1 2|grep -q 'IPMI Messaging.*enabled'"
       }
