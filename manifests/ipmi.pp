0d84dda50a51aa87453b80c9111d02a42b9c8aae
Dominique Quatravaux <dom@tsf-428-wpa-4-181.epfl.ch>
2015-07-28 19:25:01 +0200

    Set IPMI password from Puppet for great justice

---
+++ manifests/ipmi.pp
@@ -0,0 +1,10 @@
+# Class: epflsti_coreos::ipmi
+#
+# Configure IPMI with a root password and so on.
+class epflsti_coreos::ipmi() {
+  exec { "Set IPMI root password":
+    command => "ipmitool user set name 2 root && ipmitool user set password 2 ${::ipmi_root_password} && ipmi channel setaccess 1 2 link=on ipmi=on callin=on privilege=4 && ipmitool user enable 2",
+    path => $path,
+    unless => "ipmitool user list|grep -q root"
+  }
+}
