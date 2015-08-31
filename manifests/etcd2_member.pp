ecfa27616d9d917fc9a62ac907238e94cdaaf589
Dominique Quatravaux <dominique@quatravaux.org>
2015-08-31 20:08:39 +0200

    Complete the template.
    * Pretty-print ETCD_INITIAL_CLUSTER and ETCD_INITIAL_CLUSTER_STATE
    * Change data structure from $member_ips (an array) to $members (a dict)

--- manifests/etcd2_member.pp
+++ manifests/etcd2_member.pp
@@ -1,17 +1,24 @@
-# [*$::member_ips*]
-#   A list of etcd2 quorum member IP addresses. Note:
+# [*$::members*]
+#   A dict associating etcd2 quorum member names with their peer-advertised URLs. Note:
 #   etcd2 manages the quorum by itself; just changing the variable in
 #   Foreman will *not* update the quorum. You need to follow
 #   https://github.com/coreos/etcd/blob/master/Documentation/runtime-configuration.md,
 #   of which Foreman only manages the second step of "Add a New
-#   Member" (i.e. ensuring that --initial-cluster and
+#   Member" (i.e. epflsti_coreos/20-etcd2.conf.erb ensures that --initial-cluster and
 #   --initial-cluster-state are set correctly)
 class epflsti_coreos::etcd2_member(
-  $member_ips = undef,
+  $members = undef,
 ) {
-  validate_array($member_ips)
-  if (empty(intersection([$::ipaddress_ethbr4], $member_ips))) {
-    $member_ips_text = join($member_ips, ", ")
-    fail("My IP address ${::ipaddress_ethbr4} is not configured in Puppet as a member (members are: ${member_ips_text})")
+  validate_hash($members)
+  $members_as_text = join(values($members), ", ")
+  if (empty(intersection([$::ipaddress_ethbr4], values($members)))) {
+    fail("My IP address ${::ipaddress_ethbr4} is not configured in Puppet as a member (members are: ${members_as_text})")
+  }
+  file { "/etc/systemd/system/etcd2.service.d":
+    ensure => "directory"
+  } ->
+  file { "/etc/systemd/system/etcd2.service.d/20-puppet.conf":
+      ensure => "present",
+      content => template("epflsti_coreos/20-etcd2.conf.erb")
   }
 }
