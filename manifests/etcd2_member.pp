92aa9e919c47d4773eaa4a95f5f7fd6829245c55
Dominique Quatravaux <dominique@quatravaux.org>
2015-08-31 19:29:07 +0200

    New class etcd2_member (WIP)
    * Class variable for the quorum list
    * When the class is used, ensure that the IP address of the agent belongs to that list

---
+++ manifests/etcd2_member.pp
@@ -0,0 +1,16 @@
+# [*$::member_ips*]
+#   A list of etcd2 quorum member IP addresses. Note:
+#   etcd2 manages the quorum by itself; just changing the variable in
+#   Foreman will *not* update the quorum. You need to follow
+#   https://github.com/coreos/etcd/blob/master/Documentation/runtime-configuration.md,
+#   of which Foreman only manages the second step of "Add a New
+#   Member" (i.e. ensuring that --initial-cluster and
+#   --initial-cluster-state are set correctly)
+class epflsti_coreos::etcd2_member(
+  $member_ips = undef,
+) {
+  validate_array($member_ips)
+  if (empty(intersection([$::ipaddress_ethbr4], $member_ips))) {
+    fail("My IP address ${::ipaddress_ethbr4} is not configured in Puppet as a member (members are: ${member_ips})")
+  }
+}
