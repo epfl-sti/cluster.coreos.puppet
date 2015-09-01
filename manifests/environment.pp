01305088ec1b06c6940f55f63ccd60ebbc24e23e
Dominique Quatravaux <dom@tsf-428-wpa-0-078.epfl.ch>
2015-09-01 19:22:33 +0200

    /etc/environment, redux
    * Rename class epflsti_coreos::environment
    * Access /etc/environment through /opt/root, as per https://github.com/epfl-sti/cluster.coreos.puppet/commit/0219a337c3843a9a7989422c1150b2c2c30b5805

--- manifests/environment.pp
+++ manifests/environment.pp
@@ -1,6 +1,8 @@
+# Maintain /etc/environment from facts
+#
 # Class to load on all nodes
-class epflsti_coreos {
-  file { "/etc/environment":
+class epflsti_coreos::environment {
+  file { "/opt/root/etc/environment":
       ensure => "present",
       content => template("epflsti_coreos/environment.erb")
   }
