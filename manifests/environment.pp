d20c5e858682d260b34bb770ed3c7722110ea2cc
Dominique Quatravaux <dom@tsf-452-wpa-5-089.epfl.ch>
2015-09-02 17:33:13 +0200

    Remove CHEAT
    * Hopefully passed everywhere by now
    * Prevents provisioning, because the first Puppet run (after install,
      before reboot) can hardly restart itself

--- manifests/environment.pp
+++ manifests/environment.pp
@@ -6,15 +6,4 @@ class epflsti_coreos::environment {
       ensure => "present",
       content => template("epflsti_coreos/environment.erb")
   }
-
-  # CHEAT: update /etc/systemd/system/puppet.service without re-installing
-  file { "/etc/systemd/system/puppet.service":
-    ensure => "present",
-    content => template("epflsti_coreos/puppet.service.erb")
-  } ~>
-  exec { "restart Puppet from Puppet":
-    command => "systemctl daemon-reload && systemctl restart puppet.service",
-    path => $::path,
-    refreshonly => true
-  }
 }
