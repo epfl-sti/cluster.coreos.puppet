a494a669cfb6c4173fb068563142c4c106efd125
CoreOS Admin <core@c35.ne.cloud.epfl.ch>
2015-09-10 14:26:33 +0000

    Add has_ups custom fact

--- manifests/hostvars.pp
+++ manifests/hostvars.pp
@@ -1,7 +1,23 @@
 # Configure things that change depending on the host.
 #
 # This class is intended to be loaded on all nodes
-class epflsti_coreos::hostvars {
+# === Parameters:
+#
+# [*ups_hosts*]
+#   A list of short hostnames that have uninterruptible power plugged into them.
+#
+# === Actions:
+#
+# * Create /etc/facter/facts.d/epflsti.txt to override ipaddress
+#   (working around
+#   https://ask.puppetlabs.com/question/5112/how-to-force-facter-not-to-use-private-ip-address/) and provide a custom fact "has_ups"
+# 
+
+class epflsti_coreos::hostvars(
+  $ups_hosts = [],
+) {
+  $has_ups = member($::hostname, $ups_hosts)
+
   # Custom facts
   file { "/etc/facter":
     ensure => "directory"
