46dea01fc8d8b96af4940ae708af6a27d42637b0
CoreOS Admin <core@c35.ne.cloud.epfl.ch>
2015-09-11 13:39:44 +0000

    Manage SSH keys with Puppet
    * New class epflsti_coreos::ssh

---
+++ manifests/ssh.pp
@@ -0,0 +1,26 @@
+# Class: epflsti_coreos::ssh
+#
+# Configure ssh access to bare metal.
+#
+# Use public keys (no passwords) just like
+# https://coreos.com/os/docs/latest/cloud-config.html#ssh_authorized_keys,
+# except you can add / remove administrators without reinstalling the
+# entire fleet.
+#
+# === Global Variables:
+#
+# [*$::ssh_authorized_keys*]
+#
+#   The list of SSH authorized keys, as a comma-separated string. Uses
+#   a global variable so that we can keep using the cloud-config mechanism
+#   for access *during* bootstrap.
+#
+# === Actions:
+#
+# Update /home/core/.ssh/authorized_keys.d/coreos-cloudinit
+class epflsti_coreos::ssh {
+  file { "/opt/root/home/core/.ssh/authorized_keys.d/coreos-cloudinit":
+    ensure => "present",
+    content => template("epflsti_coreos/ssh_authorized_keys.erb")
+  }
+}
