0c6d80d4afb48da4d9d7a4e831b2cc3c4e25c005
Dominique Quatravaux <dominique@quatravaux.org>
2015-09-22 18:46:52 +0200

    Split out steps in big ipmitool script
    * Can set and check user name separately
    * Can set and check password separately

--- manifests/ipmi.pp
+++ manifests/ipmi.pp
@@ -4,9 +4,19 @@
 class epflsti_coreos::ipmi() {
   case $::boardproductname {
     "X7DBT": {
+      exec { "Create user root":
+        command => "ipmitool user set name 2 root",
+        path => $path,
+        unless => "ipmitool user list |grep '^2.*root'"
+      } ->
+      exec { "Set up IPMI password":
+        command => "ipmitool user set password 2 ${::ipmi_root_password}",
+        path => $path,
+        unless => "ipmitool user test 2 16 ${::ipmi_root_password}"
+      } ->
       # Believe it or not, "ipmi=on" actually turns it off! "ipmi=zoinx" works.
       exec { "Set up IPMI remote access":
-        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool user set name 2 root&& ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2  && ipmitool user set password 2 ${::ipmi_root_password}",
+        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2",
         path => $path,
         unless => "ipmitool channel getaccess 1 2|grep -q 'IPMI Messaging.*enabled'"
       }
