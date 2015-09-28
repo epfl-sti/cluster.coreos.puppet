a3e19f7e7daf7b3d1ff76d85986f7dc51bdfccce
Dominique Quatravaux <dom@tsf-452-wpa-7-106.epfl.ch>
2015-09-28 20:14:22 +0200

    Shorter names needed for bridges

--- manifests/tenant_networking/private.pp
+++ manifests/tenant_networking/private.pp
@@ -1,5 +1,12 @@
 class epflsti_coreos::tenant_networking::private {
-  define tenant($ipv6_subnet, $ipv6_netmask = "80"){
+  define tenant(
+    $br_name = "br-${name}",
+    $ipv6_subnet,
+    $ipv6_netmask = "80") {
+    validate_string($br_name)
+    validate_slength($br_name, 15)
+    validate_string($ipv6_subnet)
+
     file { "/etc/systemd/network/ethbr-${name}.netdev":
       ensure => "present",
       content => inline_template("[NetDev]\nName=ethbr-${name}\nKind=bridge\n")
