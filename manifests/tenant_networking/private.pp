ade4d6f0ebf0874d9c8f08d03497e5ba1a2fa46a
Dominique Quatravaux <dom@tsf-452-wpa-7-106.epfl.ch>
2015-09-28 20:07:18 +0200

    Restart networkd upon changing tenant

--- manifests/tenant_networking/private.pp
+++ manifests/tenant_networking/private.pp
@@ -3,6 +3,12 @@ class epflsti_coreos::tenant_networking::private {
     file { "/etc/systemd/network/ethbr-${name}.netdev":
       ensure => "present",
       content => inline_template("[NetDev]\nName=ethbr-${name}\nKind=bridge\n")
-    }
+    } ~> Exec["restart networkd for tenant_networking"]
   }
+
+  exec { "restart networkd for tenant_networking":
+    command => "/usr/bin/systemctl restart systemd-networkd.service",
+    refreshonly => true
+  }
+
 }
