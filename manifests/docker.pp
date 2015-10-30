e0172b65c0af62d419952173bcbdb5d1ec0432c3
Dominique Quatravaux <dominique@quatravaux.org>
2015-10-30 15:14:25 +0100

    Use private (in)secure registry
    * Add --insecure-registry docker-registry.ne.cloud.epfl.ch:5000
      on all dockerd's command lines

--- manifests/docker.pp
+++ manifests/docker.pp
@@ -2,5 +2,19 @@
 #
 # Special docker tweaks for EPFL-STI clusters
 #
+# === Actions:
+#
+# * Adds --insecure-registry docker-registry.ne.cloud.epfl.ch:5000
+#   on the command line of all dockerd's
+#
+
 class epflsti_coreos::docker() {
+  file { "/etc/systemd/system/docker.service.d/50-insecure-private-registry.conf":
+      ensure => "present",
+      content => template("epflsti_coreos/50-insecure-private-registry.conf.erb")
+    } ~>
+    exec { "restart docker in host":
+      command => "/usr/bin/systemctl daemon-reload && /usr/bin/systemctl restart docker.service",
+      refreshonly => true
+    }  
 }
