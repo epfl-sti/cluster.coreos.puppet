ca09255a7d00ab5fd4adb2ec03ce8657fe5d8a6c
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-11 17:55:30 +0100

    Fix bogus syntax

--- manifests/deis.pp
+++ manifests/deis.pp
@@ -48,7 +48,7 @@ class epflsti_coreos::deis() {
     path => $::path,
     require => File["${rootpath}/opt/bin"]
   } ~> exec { "create /etc/deis-release":
-    command => "${rootpath}/opt/bin/deisctl --version > ${rootpath/etc/deis-release}",
+    command => "${rootpath}/opt/bin/deisctl --version > ${rootpath}/etc/deis-release",
     path => $::path,
     refreshonly => true
   }
