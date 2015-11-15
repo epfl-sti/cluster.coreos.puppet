865613b39843a83a42b96181a9e1144c7206f0f9
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-15 23:56:01 +0100

    Moar networking
    * Extract $_primary_interface from the Foreman ENC info
    * Disable DHCPv4 on the other interface, closing a long-standing bug

--- manifests/private/networking.pp
+++ manifests/private/networking.pp
@@ -13,6 +13,9 @@
 class epflsti_coreos::private::networking {
   include ::epflsti_coreos::private::systemd
 
+  $_primary_interface = inline_template(
+    '<%= @foreman_interfaces.first{|i| i.primary}["identifier"] %>')
+
   systemd::unit { "ethbr4.netdev":
     content => join([
                      "[NetDev]\n",
@@ -23,6 +26,27 @@ class epflsti_coreos::private::networking {
 
   systemd::unit { "50-ethbr4-internal.network":
     content => template("epflsti_coreos/networking/50-ethbr4-internal.network.erb")
+  }
+
+  systemd::unit { "00-${_primary_interface}.network":
+    content => join([
+              "[Match]\n",
+              "Name=${_primary_interface}\n",
+              "\n",
+              "[Network]\n",
+              "DHCP=no\n",
+              "Bridge=ethbr4\n"
+                     ])
+  }
+
+  systemd::unit { "70-leave-unconfigured.network":
+    content => join([
+              "[Match]\n",
+              "Name=enp*\n",
+              "\n",
+              "[Network]\n",
+              "DHCP=no\n"
+                     ])
   }
 
 }
