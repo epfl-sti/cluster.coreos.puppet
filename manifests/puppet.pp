1cd5615360b1833b6a369a3559004c14ccb76a60
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-15 20:11:23 +0100

    epflsti_coreos::puppet: $has_ipmi = true by default

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -4,7 +4,9 @@
 #
 # Note that we are talking about the steady-state Puppet here; not the
 # bootstrap run before reboot. See ../README.md for details.
-class epflsti_coreos::puppet() {
+class epflsti_coreos::puppet(
+  $has_ipmi = true
+  ) {
   $rootpath = "/opt/root"
   include ::epflsti_coreos::private::systemd
 
