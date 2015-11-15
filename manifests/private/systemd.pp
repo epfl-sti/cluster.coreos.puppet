3da9bac1cd1db9c976c9fbd861af0219c87f4cb7
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-15 19:57:55 +0100

    Shotgun into yet another Puppet syntax sillyness

--- manifests/private/systemd.pp
+++ manifests/private/systemd.pp
@@ -1,29 +1,31 @@
 # Library for dealing with systemd in EPFL-STI clusters
 
-define unit (
-  $enable = true,
-  $start = true,
-  $content = undef
-) {
-
-  file { "/etc/systemd/system/${name}":
-    content => $content,
-  }
-
-  if ($enable) {
-    exec { "Enabling ${name} in systemd":
-      command => "/usr/bin/systemctl enable ${name}",
-      path => $::path,
-      unless => "/usr/bin/systemctl is-enabled ${name}",
-      require => File["/etc/systemd/system/${name}"]
-    }  
-  }
-
-  if ($start and $::lifecycle_stage == "production") {
-    exec { "Restarting ${name} in systemd":
-      command => "/usr/bin/systemctl daemon-reload && /usr/bin/systemctl reload-or-restart ${name}",
-      path => $::path,
-      require => File["/etc/systemd/system/${name}"]
+class epflsti_coreos::private::systemd {
+  define unit (
+    $enable = true,
+    $start = true,
+    $content = undef
+  ) {
+  
+    file { "/etc/systemd/system/${name}":
+      content => $content,
+    }
+  
+    if ($enable) {
+      exec { "Enabling ${name} in systemd":
+        command => "/usr/bin/systemctl enable ${name}",
+        path => $::path,
+        unless => "/usr/bin/systemctl is-enabled ${name}",
+        require => File["/etc/systemd/system/${name}"]
+      }  
+    }
+  
+    if ($start and $::lifecycle_stage == "production") {
+      exec { "Restarting ${name} in systemd":
+        command => "/usr/bin/systemctl daemon-reload && /usr/bin/systemctl reload-or-restart ${name}",
+        path => $::path,
+        require => File["/etc/systemd/system/${name}"]
+      }
     }
   }
 }
