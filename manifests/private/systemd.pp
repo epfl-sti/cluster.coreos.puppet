2b09e2149573963cd406f89f308f7be7b9538084
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-15 22:42:55 +0100

    Classify service, network & netdev in the right directories

--- manifests/private/systemd.pp
+++ manifests/private/systemd.pp
@@ -7,19 +7,25 @@ class epflsti_coreos::private::systemd {
     $content = undef
   ) {
   
+    if ($name =~ /\.(service)$/) {
+      $_kind = "service"
+    } elsif ($name =~ /\.(network|netdev)$) {
+      $_kind = "network"
+    } else {
+      fail("Cannot determine unit type for ${name}")
+    }
+  
     if ($content == undef) {
       $_file_prereqs = []
     } else {
-      file { "/etc/systemd/system/${name}":
+      file { "/etc/systemd/${_kind}/${name}":
         content => $content,
       }
-      $_file_prereqs = File["/etc/systemd/system/${name}"]
+      $_file_prereqs = File["/etc/systemd/${_kind}/${name}"]
     }
 
-    $_is_service = !(!($name =~ /\.service$/))
-  
     if ($enable == undef) {
-      $_do_enable = $_is_service
+      $_do_enable = $_kind == "service"
     } else {
       $_do_enable = $enable
     }
@@ -36,7 +42,7 @@ class epflsti_coreos::private::systemd {
     if ($::lifecycle_stage == "bootstrap") {
       $_do_start = false
     } elsif ($start == undef) {
-      $_do_start = $_is_service
+      $_do_start = $_kind == "service"
     } else {
       $_do_start = $start
     }
