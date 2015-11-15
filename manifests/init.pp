2e12d1fba478ba2996fca8248361f0df4bd6c182
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-15 10:36:46 +0100

    Introduce a top-level class.
    This will make it easier to deal with the "bootstrap"
    $::lifecycle_stage, as only that class needs be aware of that.

---
+++ manifests/init.pp
@@ -0,0 +1,12 @@
+# Main class for EPFL-STI CoreOS clusters.
+#
+# This class is meant to be invoked directly by Foreman (or whatever
+# top-level configuration mechanism is in use). Only a handful classes
+# that cannot be configured in software (such as gateway), should
+# also be invoked directly; all others are invoked from here.
+
+class epflsti_coreos() {
+  class { "epflsti_coreos::ipmi": }
+  class { "epflsti_coreos::ssh": }
+  class { "epflsti_coreos::etcd2": }
+}
