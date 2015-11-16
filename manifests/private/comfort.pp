f07309c00f1bcb66838f1c16b2984c4f8ebe9b85
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-16 01:58:41 +0100

    Use numeric groups
    User "core" is not visible from inside Docker

--- manifests/private/comfort.pp
+++ manifests/private/comfort.pp
@@ -2,14 +2,16 @@
 #
 class epflsti_coreos::private::comfort() {
   file { "/home/core/.toolboxrc":
-    owner => "core",
+    owner => 500,
+    group => 500,
     content => "TOOLBOX_DOCKER_IMAGE=epflsti/cluster.coreos.toolbox
 TOOLBOX_DOCKER_TAG=latest
 "
   }
 
   file { "/home/core/.bash_history":
-    owner => "core",
+    owner => 500,
+    group => 500,
     replace => false,
     content => "fleetctl list-units
 fleetctl list-machines
@@ -23,6 +25,7 @@ docker exec puppet.service puppet agent -t
 "
   }
 
+  $rootpath = "/opt/root"
   file { "/opt/bin/fleetcheck":
     mode => '0755',
     content => template("epflsti_coreos/fleetcheck.erb")
