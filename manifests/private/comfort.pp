4ae3a1c3a4d9e28dd74c9b16ba8793bd66840bcc
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-16 01:33:59 +0100

    Don't use fancy-schmancy here-docs
    Multiline is just as good

--- manifests/private/comfort.pp
+++ manifests/private/comfort.pp
@@ -3,10 +3,24 @@
 class epflsti_coreos::private::comfort() {
   file { "/home/core/.toolboxrc":
     owner => "core",
-    content => @("TOOLBOXRC")
-      TOOLBOX_DOCKER_IMAGE=epflsti/cluster.coreos.toolbox
-      TOOLBOX_DOCKER_TAG=latest
-      | TOOLBOXRC
+    content => "TOOLBOX_DOCKER_IMAGE=epflsti/cluster.coreos.toolbox
+TOOLBOX_DOCKER_TAG=latest
+"
+  }
+
+  file { "/home/core/.bash_history":
+    owner => "core",
+    replace => false,
+    content => "fleetctl list-units
+fleetctl list-machines
+etcdctl member list
+etcdctl cluster-health
+journalctl -xe
+journalctl -l
+systemctl list-unit-files
+systemctl cat puppet.service
+docker exec puppet.service puppet agent -t
+"
   }
 
   file { "/opt/bin/fleetcheck":
