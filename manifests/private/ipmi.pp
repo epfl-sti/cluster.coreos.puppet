b75fb79c27a1feab20d6eb81b3448d58a06bfb28
Dominique Quatravaux <dominique@quatravaux.org>
2015-11-16 19:28:57 +0100

    Make host /dev available in Puppet
    Rationale:
    * We need to be coherent between production and bootstrap, so that we don't have to pull random symlinks around
    * We need /dev/null etc. at least, so we might just as well use the whole thing

--- manifests/private/ipmi.pp
+++ manifests/private/ipmi.pp
@@ -7,11 +7,8 @@ class epflsti_coreos::private::ipmi() {
   exec { "insert IPMI modules and wait for /dev/ipmi0 to appear":
     command => "/usr/sbin/chroot ${rootpath} /bin/bash -e -x -c 'modprobe ipmi_si; modprobe ipmi_devintf; for i in \$(seq 1 100); do test -c /dev/ipmi0 && exit 0; sleep 0.1; set +x; done; exit 2'",
     path => $::path,
-    creates => "${rootpath}/dev/ipmi0"
-  } -> file { "/dev/ipmi0":
-    ensure => "link",
-    target => "${rootpath}/dev/ipmi0"
-  }
+    creates => "/dev/ipmi0",
+  } -> anchor { "dev_ipmi0_available": }
 
   case $::boardproductname {
     "X7DBT": {
@@ -19,20 +16,20 @@ class epflsti_coreos::private::ipmi() {
         command => "ipmitool user set name 2 root",
         path => $path,
         unless => "ipmitool user list |grep '^2.*root'",
-        require => File["/dev/ipmi0"]
+        require => Anchor["dev_ipmi0_available"]
       } ->
       exec { "Set up IPMI password":
         command => "ipmitool user set password 2 ${::ipmi_root_password}",
         path => $path,
         unless => "ipmitool user test 2 16 ${::ipmi_root_password}",
-        require => File["/dev/ipmi0"]
+        require => Anchor["dev_ipmi0_available"]
       } ->
       # Believe it or not, "ipmi=on" actually turns it off! "ipmi=zoinx" works.
       exec { "Set up IPMI remote access":
         command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2",
         path => $path,
         unless => "ipmitool channel getaccess 1 2|grep -q 'IPMI Messaging.*enabled'",
-        require => File["/dev/ipmi0"]
+        require => Anchor["dev_ipmi0_available"]
       }
       # When the System Event Log (SEL) is full, we get a boot-time message
       # to the tune of: SEL full, press F1 to continue :-(
@@ -40,7 +37,7 @@ class epflsti_coreos::private::ipmi() {
         path => $path,
         command => "/bin/false",
         unless => "ipmitool sel clear",
-        require => File["/dev/ipmi0"]
+        require => Anchor["dev_ipmi0_available"]
       }
     }
     # TODO: auto-configure our X8DTT's too (they have all been configured manually)
