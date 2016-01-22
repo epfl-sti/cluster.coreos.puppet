47ba6c5e2bcc7d8e947eae2a7d3423a19cb06d22
root <root@puppetmaster.ne.cloud.epfl.ch>
2016-01-22 16:57:22 +0000

    Add ensure => "absent" to delete Deis assets
    Note: global settings remain in the cluster; one must still
      * fleetctl destroy everything with deis in the name,
      * etcdctl rm --recursive /deis

--- manifests/private/deis.pp
+++ manifests/private/deis.pp
@@ -13,55 +13,79 @@
 # [*rootpath*]
 #    Where in the Puppet-agent Docker container, the host root is mounted
 #
+# [*ensure*]
+#    Either "present" or "absent"
+#
 
 class epflsti_coreos::private::deis(
-  $rootpath = $epflsti_coreos::private::params::rootpath
+  $rootpath = $epflsti_coreos::private::params::rootpath,
+  $ensure = "present"
 )
 inherits epflsti_coreos::private::params {
   include ::epflsti_coreos::private::systemd
 
-  ensure_resource('file',
-    ["${rootpath}/opt", "${rootpath}/opt/bin"],
-    { ensure => "directory" })
-
-  define opt_bin_script() {
-    file { "${::epflsti_coreos::private::deis::rootpath}/opt/bin/${name}":
+  define script(
+    $in_subdir
+  ) {
+    file { "${::epflsti_coreos::private::deis::rootpath}${in_subdir}/${name}":
       mode => "0755",
+      ensure => "file",
       content => template("epflsti_coreos/deis/${name}.erb"),
-      require => File["${::epflsti_coreos::private::deis::rootpath}/opt/bin"]
+      require => File["${::epflsti_coreos::private::deis::rootpath}${in_subdir}"]
     }
   }
+  
 
-  define run_deis_bin_script() {
-    file { "${::epflsti_coreos::private::deis::rootpath}/run/deis/bin/${name}":
-      mode => "0755",
-      content => template("epflsti_coreos/deis/${name}.erb"),
-      require => File["${::epflsti_coreos::private::deis::rootpath}/run/deis/bin"]
+  if ($ensure == "absent") {
+    ensure_resource('file',
+        ["${rootpath}/run/deis", "${rootpath}/etc/deis",
+         "${rootpath}/var/lib/deis"],
+        {
+          ensure => "absent",
+          force => true
+        })
+    file { ["${rootpath}/opt/bin/deisctl", "${rootpath}/etc/deis-release"]:
+     ensure => "absent"
     }
-  }
+  } else {
+    ensure_resource('file',
+        ["${rootpath}/opt", "${rootpath}/opt/bin",
+         "${rootpath}/run/deis", "${rootpath}/run/deis/bin"],
+        {
+          ensure => "directory"
+        })
 
-  opt_bin_script {
-    ["wupiao", "download-k8s-binary", "deis-graceful-shutdown",
-     "scheduler-policy.json", "deis-debug-logs"]:
-  }
+    script {
+      ["wupiao", "download-k8s-binary", "deis-graceful-shutdown",
+       "scheduler-policy.json", "deis-debug-logs"]:
+         in_subdir => "/opt/bin"
+    }
 
-  file { ["${rootpath}/run/deis", "${rootpath}/run/deis/bin"]:
-    ensure => "directory",
-  } -> run_deis_bin_script { ["get_image", "preseed"]: }
+    script { ["get_image", "preseed"]: 
+         in_subdir => "/run/deis/bin"
+    }
+
+    exec { "install deisctl":
+      creates => "${rootpath}/opt/bin/deisctl",
+      command => "wget -O - http://deis.io/deisctl/install.sh | chroot /opt/root /usr/bin/bash -s",
+      path => $::path,
+      require => File["${rootpath}/opt/bin"]
+    } ~> exec { "create /etc/deis-release":
+      command => "/bin/sh -c 'echo -n \"DEIS_RELEASE=v\"; ${rootpath}/opt/bin/deisctl --version' > ${rootpath}/etc/deis-release",
+      path => $::path,
+      refreshonly => true
+    }
+  }  # $ensure != "absent"
 
   private::systemd::unit { "graceful-deis-shutdown.service":
-    content => template('epflsti_coreos/deis/graceful-deis-shutdown.service.erb'),
+    content => $ensure ? {
+     "absent" => undef,
+     default  => template('epflsti_coreos/deis/graceful-deis-shutdown.service.erb')
+    },
+    ensure => $ensure ? {
+     "absent" => "absent",
+     default  => undef
+    },
     start => false
-  }
-
-  exec { "install deisctl":
-    creates => "${rootpath}/opt/bin/deisctl",
-    command => "wget -O - http://deis.io/deisctl/install.sh | chroot /opt/root /usr/bin/bash -s",
-    path => $::path,
-    require => File["${rootpath}/opt/bin"]
-  } ~> exec { "create /etc/deis-release":
-    command => "/bin/sh -c 'echo -n \"DEIS_RELEASE=v\"; ${rootpath}/opt/bin/deisctl --version' > ${rootpath}/etc/deis-release",
-    path => $::path,
-    refreshonly => true
   }
 }
