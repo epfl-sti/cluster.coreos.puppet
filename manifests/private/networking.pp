2932a88d24208bb8c4f5ab7b72e679ca9a4be22b
root <root@puppetmaster.ne.cloud.epfl.ch>
2016-04-19 21:08:46 +0000

    Improve networking.pp
    * Proper $rootpath handling
    * Enumerate all internal interfaces
    * Pick MAC address of first one to use as ethbr4's MAC address
    * Lay down roadmap for bonding support

--- manifests/private/networking.pp
+++ manifests/private/networking.pp
@@ -1,55 +1,74 @@
 # Configure networking pre-bootstrap.
 #
-# The primary physical interface of each CoreOS node is bridged on a
-# bridge device called ethbr4 ("Ethernet bridge for IPv4"). This is so
-# that Docker containers (most notably, the Foreman / Puppet master)
-# may claim an IP address on the internal (RFC1918) IPv4 network. If
-# the container moves, that IP (called a "Virtual IP", or VIP) can
-# stay the same, and the clients will catch up (after their ARP
-# timeout expires) with zero reconfiguration.
+# The physical interfaces of each CoreOS node are partitioned into
+# *internal* (tied to the internal network - The vast majority of
+# them) and *external* (tied to the EPFL or other public IPv4
+# network). Nodes that have at least one external interface are called
+# "gateway" nodes; they receive an additional layer of network
+# configuration (see ../gateway.pp) which takes precedence over the
+# configuration in this class thanks to networkd's cascading
+# preferences mechanism.
 #
-# Note that gateway nodes receive an additional layer of network
-# configuration; see ../gateway.pp. The interaction between the
-# two is managed using networkd's cascading preferences mechanism.
+# Internal interfaces (assuming there are more than one) are bonded
+# together in a zero-SPOF active/passive ("mode=1") configuration, and
+# then bridged into a device called ethbr4 ("Ethernet bridge for
+# IPv4"). This is so that Docker containers (most notably, the Foreman
+# / Puppet master) may claim an IP address on the internal (RFC1918)
+# IPv4 network. If the container moves, that IP (called a "Virtual
+# IP", or VIP) can stay the same, and the clients will catch up (after
+# their ARP timeout expires) with zero reconfiguration.
 #
 # === Actions:
 #
 # * Set the FQDN in /etc/hostname, as is the CoreOS way
 #
-# * Set up the ethbr4 bridge as explained above, using the
-#   Foreman-specified primary interface and IPv4 address
+# * [TODO] Set up the bond0 interface and ethbr4 bridge as explained
+#   above, using the Foreman-specified primary interface and IPv4
+#   address, and the MAC address of the first (internal) physical
+#   interface
 #
 # * Set up the default route and DNS server, in a way that lets
 #   gateway.pp override the former seamlessly
 #
-# * Disable DHCPv4 on all physical interfaces, primary or not, so as
-#   to save valuable DHCP address space
+# * Disable DHCPv4 on all interfaces
 #
 # === Parameters:
 #
-# [*primary_interface*]
-#   The name of the network interface connected to the internal network
+# [*rootpath*]
+#    Where in the Puppet-agent Docker container, the host root is
+#    mounted
 #
 # === Bootstrapping:
 #
 # This class is bootstrap-safe, as it only changes files and doesn't
-# restart networkd.
+# restart networkd. The obvious downside (or is it a feature?) is that
+# "on-the-fly" network changes in production state (post-bootstrap)
+# have no effect, until one restarts networkd by hand.
 
 class epflsti_coreos::private::networking(
-  $primary_interface = $::epflsti_coreos::private::params::primary_interface
-)
-inherits epflsti_coreos::private::params {
+  $rootpath = $::epflsti_coreos::private::params::rootpath
+) inherits epflsti_coreos::private::params {
+
   include ::epflsti_coreos::private::systemd
 
-  file { "${::epflsti_coreos::private::params::rootpath}/etc/hostname":
+  file { "${rootpath}/etc/hostname":
     content => "${::fqdn}\n"
   }
 
+  $internal_interfaces = delete(grep(split($::interfaces, ','), '^(en|eth)'), ["ethbr4", $::epflsti_coreos::gateway::external_interface])
+  validate_bool(size($internal_interfaces) > 0)
+  $first_internal_interface = $internal_interfaces[0]
+  $first_mac_address = inline_template("<%= scope.lookupvar('macaddress_' + @first_internal_interface) %>")
+
   systemd::unit { "ethbr4.netdev":
-    content => "[NetDev]
+    content => inline_template("#
+# Managed by Puppet, DO NOT EDIT
+#
+[NetDev]
 Name=ethbr4
 Kind=bridge
-"
+MACAddress=<%= @first_mac_address %>
+")
   }
 
   systemd::unit { "50-ethbr4-internal.network":
