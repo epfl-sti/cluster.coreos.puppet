652be5a030041c3d281b1c217f5f49e59e1e0ef9
root <root@puppetmaster.ne.cloud.epfl.ch>
2016-09-01 17:05:54 +0000

    Generalize to X8DTT
    * All commands are the same, except for the root user ID
    * That includes the "zoinx" gizmo - Tested as necessary as well on an X8DDT

--- manifests/private/ipmi.pp
+++ manifests/private/ipmi.pp
@@ -29,34 +29,38 @@ class epflsti_coreos::private::ipmi() {
 
   case $::boardproductname {
     "X7DBT": {
-      exec { "Create user root":
-        command => "ipmitool user set name 2 root",
-        path => $::path,
-        unless => "ipmitool user list |grep '^2.*root'",
-        require => Anchor["dev_ipmi0_available"]
-      } ->
-      exec { "Set up IPMI password":
-        command => "ipmitool user set password 2 ${::ipmi_root_password}",
-        path => $::path,
-        unless => "ipmitool user test 2 16 ${::ipmi_root_password}",
-        require => Anchor["dev_ipmi0_available"]
-      } ->
-      # Believe it or not, "ipmi=on" actually turns it off! "ipmi=zoinx" works.
-      exec { "Set up IPMI remote access":
-        command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool channel setaccess 1 2 link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable 2",
-        path => $::path,
-        unless => "ipmitool channel getaccess 1 2|grep -q 'IPMI Messaging.*enabled'",
-        require => Anchor["dev_ipmi0_available"]
-      }
-      # When the System Event Log (SEL) is full, we get a boot-time message
-      # to the tune of: SEL full, press F1 to continue :-(
-      exec { "Empty IPMI bit bucket":
-        path => $::path,
-        command => "/bin/false",
-        unless => "ipmitool sel clear",
-        require => Anchor["dev_ipmi0_available"]
-      }
+      $rootUserNo = 2
     }
-    # TODO: auto-configure our X8DTT's too (they have all been configured manually)
+    "X8DTT": {
+      $rootUserNo = 3
+    }
+  }
+  exec { "Create user root":
+    command => "ipmitool user set name ${rootUserNo} root",
+    path => $::path,
+    unless => "ipmitool user list 1 |grep '^${rootUserNo}.*root'",
+    require => Anchor["dev_ipmi0_available"]
+  } ->
+  exec { "Set up IPMI password":
+    command => "ipmitool user set password ${rootUserNo} ${::ipmi_root_password}",
+    path => $::path,
+    unless => "ipmitool user test ${rootUserNo} 16 ${::ipmi_root_password}",
+    require => Anchor["dev_ipmi0_available"]
+  }
+
+  exec { "Set up IPMI remote access":
+    # Believe it or not, "ipmi=on" actually turns it off on both X7DBT and X8DDT! "ipmi=zoinx" works.
+    command => "ipmitool lan set 1 auth ADMIN MD5 && ipmitool lan set 1 access on && ipmitool channel setaccess 1 ${rootUserNo} link=zoinx ipmi=zoinx callin=on privilege=4 && ipmitool user enable ${rootUserNo}",
+    path => $::path,
+    unless => "ipmitool channel getaccess 1 ${rootUserNo}|grep -q 'IPMI Messaging.*enabled'",
+    require => Exec["Set up IPMI password"]
+  }
+  # When the System Event Log (SEL) is full, we get a boot-time message
+  # to the tune of: SEL full, press F1 to continue :-(
+  exec { "Empty IPMI bit bucket":
+    path => $::path,
+    command => "/bin/false",
+    unless => "ipmitool sel clear",
+    require => Anchor["dev_ipmi0_available"]
   }
 }
