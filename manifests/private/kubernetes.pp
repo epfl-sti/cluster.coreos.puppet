94bce86c3e653447f798c3af6ffb4485273fae58
root <root@puppetmaster.ne.cloud.epfl.ch>
2016-11-02 22:33:08 +0000

    More Kubernetes progress
    * Discovered proper syntax for --etcd-servers by trial and error ☹
    * More required pods and files defined as per documentation

--- manifests/private/kubernetes.pp
+++ manifests/private/kubernetes.pp
@@ -20,7 +20,7 @@
 #    Whether this is a Kubernetes master node.
 #
 class epflsti_coreos::private::kubernetes(
-  $k8s_version = "1.4.5",
+  $k8s_version = "1.4.3",
   $kubernetes_masters = $::epflsti_coreos::private::params::kubernetes_masters,
   $rootpath = $::epflsti_coreos::private::params::rootpath
 ) inherits epflsti_coreos::private::params {
@@ -41,6 +41,7 @@ class epflsti_coreos::private::kubernetes(
     $_master_address = $kubernetes_masters[0]
   }
 
+  $kube_quay_version = "v${k8s_version}_coreos.0"
   systemd::unit { "kubernetes.service":
       content => "[Unit]
 Description=Kubernetes in a box (Docker)
@@ -51,7 +52,7 @@ Requires=docker.service calico-node.service calico-libnetwork.service
 ExecStartPre=/usr/bin/mkdir -p /etc/kubernetes/manifests
 ExecStartPre=/usr/bin/mkdir -p /var/log/containers
 
-Environment=KUBELET_VERSION=v${k8s_version}_coreos.0
+Environment=KUBELET_VERSION=${kube_quay_version}
 Environment=\"RKT_OPTS=--volume modprobe,kind=host,source=/usr/sbin/modprobe \
   --mount volume=modprobe,target=/usr/sbin/modprobe \
   --volume lib-modules,kind=host,source=/lib/modules \
@@ -68,6 +69,7 @@ ExecStart=/bin/sh -x /usr/lib/coreos/kubelet-wrapper \
   --register-schedulable=false \
   --allow-privileged=true \
   --config=/etc/kubernetes/manifests \
+  --hostname-override=${::fqdn} \
   --cluster-dns=${::dns_vip} \
   --cluster-domain=cluster.local
 
@@ -87,5 +89,134 @@ WantedBy=multi-user.target
     path => $::path,
     command => "wget -O ${_kubectl_path} http://storage.googleapis.com/kubernetes-release/release/v${k8s_version}/bin/linux/amd64/kubectl && chmod 755 ${_kubectl_path}",
     unless => "grep v${k8s_version} ${_kubectl_path}"
+  }
+
+  if ($is_master) {
+    file { "${rootpath}/etc/kubernetes/manifests":
+      ensure => "directory"
+    }
+    file { "${rootpath}/etc/kubernetes/manifests/kube-apiserver.yaml":
+      content => "apiVersion: v1
+kind: Pod
+metadata:
+  name: kube-apiserver
+  namespace: kube-system
+spec:
+  hostNetwork: true
+  containers:
+  - name: kube-apiserver
+    image: quay.io/coreos/hyperkube:${kube_quay_version}
+    command:
+    - /hyperkube
+    - apiserver
+    - --bind-address=0.0.0.0
+    - --etcd-servers=http://${::ipaddress}:2379
+    - --allow-privileged=true
+    - --service-cluster-ip-range=172.16.0.0/16
+    - --secure-port=443
+    - --advertise-address=${::ipaddress}
+    - --admission-control=NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,ResourceQuota
+    - --tls-cert-file=/etc/kubernetes/ssl/apiserver.pem
+    - --tls-private-key-file=/etc/kubernetes/ssl/apiserver-key.pem
+    - --client-ca-file=/etc/kubernetes/ssl/ca.pem
+    - --service-account-key-file=/etc/kubernetes/ssl/apiserver-key.pem
+    - --runtime-config=extensions/v1beta1=true,extensions/v1beta1/networkpolicies=true
+    ports:
+    - containerPort: 443
+      hostPort: 443
+      name: https
+    - containerPort: 8080
+      hostPort: 8080
+      name: local
+    volumeMounts:
+    - mountPath: /etc/kubernetes/ssl
+      name: ssl-certs-kubernetes
+      readOnly: true
+    - mountPath: /etc/ssl/certs
+      name: ssl-certs-host
+      readOnly: true
+  volumes:
+  - hostPath:
+      path: /etc/kubernetes/ssl
+    name: ssl-certs-kubernetes
+  - hostPath:
+      path: /usr/share/ca-certificates
+    name: ssl-certs-host
+"
+    }
+    file { "${rootpath}/etc/kubernetes/manifests/kube-proxy.yaml":
+      content => "apiVersion: v1
+kind: Pod
+metadata:
+  name: kube-proxy
+  namespace: kube-system
+spec:
+  hostNetwork: true
+  containers:
+  - name: kube-proxy
+    image: quay.io/coreos/hyperkube:${kube_quay_version}
+    command:
+    - /hyperkube
+    - proxy
+    - --master=http://127.0.0.1:8080
+    - --proxy-mode=iptables
+    securityContext:
+      privileged: true
+    volumeMounts:
+    - mountPath: /etc/ssl/certs
+      name: ssl-certs-host
+      readOnly: true
+  volumes:
+  - hostPath:
+      path: /usr/share/ca-certificates
+    name: ssl-certs-host
+"
+    }
+    file { "${rootpath}/etc/kubernetes/manifests/kube-scheduler.yaml":
+      content => "apiVersion: v1
+kind: Pod
+metadata:
+  name: kube-scheduler
+  namespace: kube-system
+spec:
+  hostNetwork: true
+  containers:
+  - name: kube-scheduler
+    image: quay.io/coreos/hyperkube:${kube_quay_version}
+    command:
+    - /hyperkube
+    - scheduler
+    - --master=http://127.0.0.1:8080
+    - --leader-elect=true
+    livenessProbe:
+      httpGet:
+        host: 127.0.0.1
+        path: /healthz
+        port: 10251
+      initialDelaySeconds: 15
+      timeoutSeconds: 1
+"
+    }
+  }  # $is_master
+
+  file { ["${rootpath}/etc/kubernetes/cni", "${rootpath}/etc/kubernetes/cni/net.d"] :
+    ensure => "directory"
+  } ->
+  file { "${rootpath}/etc/kubernetes/cni/net.d/10-calico.conf":
+    content => "{
+    \"name\": \"calico\",
+    \"type\": \"flannel\",
+    \"delegate\": {
+        \"type\": \"calico\",
+        \"etcd_endpoints\": \"localhost:2379\",
+        \"log_level\": \"none\",
+        \"log_level_stderr\": \"info\",
+        \"hostname\": \"${::ipaddress}\",
+        \"policy\": {
+            \"type\": \"k8s\",
+            \"k8s_api_root\": \"http://127.0.0.1:8080/api/v1/\"
+        }
+    }
+}"
   }
 }
