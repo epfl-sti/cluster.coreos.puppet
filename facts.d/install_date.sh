a129ca38f5d1f4cf6efa9580bc10e4ebfa146059
root <root@puppetmaster.ne.cloud.epfl.ch>
2016-11-30 18:15:01 +0000

    Use /etc as the source of install_date_*
    Looks like /etc/coreos has a set date for some reason

--- facts.d/install_date.sh
+++ facts.d/install_date.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-install_date_epoch="$(stat -c '%Z' /opt/root/etc/coreos)"
+install_date_epoch="$(stat -c '%Z' /opt/root/etc)"
 echo install_date_epoch=$install_date_epoch
 echo -n install_date_iso=
 date -Iseconds -d @$install_date_epoch
