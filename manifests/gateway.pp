ebe33f8ec40a8f1a56234769b11aac12e583419a
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-01-13 18:48:02 +0000

    Shoot down a couple of obvious bugs in gateway.pp
    * Re-start networkd whenever the expected default route is missing
    * Don't handle the meaningless case of $external_addresses being non-null,
      but $external_ipv4_gateway being unset
    * Moar comment

--- manifests/gateway.pp
+++ manifests/gateway.pp
@@ -33,7 +33,8 @@
 #   "normal" way
 #
 # [*external_ipv4_gateway*]
-#   IPv4 default route on the external network for the gateway nodes.
+#   IPv4 default route on the external network for the gateway nodes
+#   (mandatory)
 #
 # [*ipv4_outgoing_active*]
 #   True iff this is the *active* IPv4 gateway; that is, it has the
@@ -48,9 +49,11 @@
 # === Global Variables:
 #
 # [*$::gateway_vip*]
-#   The IP address that all internal nodes have set up as their default route
-#   at provisioning time. The gateway that ${ipv4_outgoing_active} sets up this IP
-#   as an alias for itself, and enables routing and masquerading.
+#   The IP address that all internal nodes *and* the inactive
+#   gateway nodes have set up as their default route at provisioning
+#   time. The active gateway node (the one that has
+#   ${ipv4_outgoing_active} set to true) sets up this IP as an alias
+#   for itself, and enables routing and masquerading.
 #
 # [*$::cluster_owner*]
 #   The prefix to set as the name to the haproxy Docker job
@@ -67,42 +70,45 @@
 # time thusly:
 #
 # * Configure $external_interface with the $external_addresses
-# * Activate IPv4 masquerading through $external_interface
 # * Change the default route to point to $external_ipv4_gateway
 #
 # Additionnally, iff $ipv4_outgoing_active is not undef:
 #
 # * Alias the ethbr4 interface to $::gateway_vip
-# * Set up IPv4 masquerading
+# * Activate IPv4 masquerading through $external_interface
 # * Set up transparent proxying on port 80
 #
 class epflsti_coreos::gateway(
   $external_interface = undef,
   $external_addresses = [],
-  $external_ipv4_gateway = undef,
+  $external_ipv4_gateway,
   $ipv4_outgoing_active = undef,
   $enable_boundary_caching = true
 ) {
   validate_string($external_interface)
+  validate_string($external_ipv4_gateway)
 
   include ::epflsti_coreos::private::systemd
 
   exec { "restart networkd in host":
-    command => "/usr/bin/systemctl restart systemd-networkd.service",
-    refreshonly => true
+    command => "/bin/true ; set -e -x; while ip route del default; do :; done; /usr/bin/systemctl restart systemd-networkd.service",
+    refreshonly => true,
+    path => $::path
   }
 
   validate_array($external_addresses)
 
   # Feature selection
   if (size($external_addresses) > 0) {
-    $_enable_gateway_routing = $external_ipv4_gateway and $ipv4_outgoing_active
     $_enable_haproxy = true
     $_enable_squid_and_transparent_proxying = $enable_boundary_caching
+    $_enable_gateway_routing = $ipv4_outgoing_active
+    $_expected_default_route = $external_ipv4_gateway
   } else {
-    $_enable_gateway_routing = false
     $_enable_haproxy = false
     $_enable_squid_and_transparent_proxying = false
+    $_enable_gateway_routing = false
+    $_expected_default_route = $gateway_vip
   }
 
   # Set up external IPv4 addresses
@@ -114,9 +120,10 @@ class epflsti_coreos::gateway(
       content => template("epflsti_coreos/networkd/50-epflnet.network.erb")
     } ~> Exec["restart networkd in host"]
   } else {
+    # Clean-up case (host is being discharged from acting as gateway)
     file { "/etc/systemd/network/50-${external_interface}-epflnet.network":
       ensure => "absent"
-    }
+    } ~> Exec["restart networkd in host"]
     exec { "Flush addresses on ${external_interface}":
       command => "/sbin/ip addr flush dev ${external_interface}",
       onlyif => "/sbin/ip addr show dev ${external_interface} | grep -q inet"
@@ -153,16 +160,18 @@ class epflsti_coreos::gateway(
       command => "/sbin/ip addr del ${::gateway_vip}/24 dev ethbr4",
       onlyif => "/sbin/ip addr show | grep -qw ${::gateway_vip}"
     } 
-    exec { "Disable default route through ${external_interface}":
-      command => "/sbin/ip route del default dev ${external_interface}",
-      onlyif => "/sbin/ip route show dev ${external_interface} | grep -q ^default"
-    }
     exec { "Disable masquerading":
       path => $path,
       command => "/sbin/iptables -t nat -D POSTROUTING -o ${external_interface} -j MASQUERADE",
       onlyif => "/sbin/iptables -t nat -L -v| grep 'MASQUERADE.*${external_interface}'"
     } 
   }
+
+  exec { "Ensure we have ${_expected_default_route} as the default route":
+      command => "true ; while route del default; do :; done",
+      path => $path,
+      unless => "/usr/bin/test \"$(/sbin/ip route | sed -n 's/default via \(\S*\) .*/\1/p')\" = \"${_expected_default_route}\"",
+    } ~> Exec["restart networkd in host"]
 
   # Set up / tear down Squid and transparent proxying
   $_iptables_spec = "-p tcp -d '!'${::ipv4_network} --dport 80 -j REDIRECT --to 3129 -w"
