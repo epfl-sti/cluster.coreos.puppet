c20b24eba664f9513f4ef89c9caf328224e132d0
ponsfrilus <ponsfrilus@gmail.com>
2017-05-03 17:05:25 +0000

    Short date fact added

--- facts.d/install_date.sh
+++ facts.d/install_date.sh
@@ -6,3 +6,7 @@ install_date_epoch="$(stat -c '%Z' /opt/root/etc)"
 echo install_date_epoch=$install_date_epoch
 echo -n install_date_iso=
 date -Iseconds -d @$install_date_epoch
+echo -n install_date_short=
+date -Idate -d @$install_date_epoch
+
+
